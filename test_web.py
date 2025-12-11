22431e2d5396deb600df54b64ea557ee7500e233
Vincentonguk <vincentonguk@gmail.com>
2025-12-11 22:40:11 +0000

    Delete test_web.py

--- test_web.py
+++
@@ -1,7 +0,0 @@
-from src.tools.web_search import web_search
-
-result = web_search("O que é RAG em IA?")
-print(result)
-from src.tools.web_search import web_search
-
-print(web_search("O que é RAG em IA?"))
